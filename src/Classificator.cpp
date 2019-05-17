6637aec444b7da797a4ab1eb5d604f6301ed5550
Felipe Kenzo <felipe.kenzo@live.com>
2019-05-17 00:41:39 -0300

    Correção de contraste parâmtros Wd

--- src/Classificator.cpp
+++ src/Classificator.cpp
@@ -27,6 +27,7 @@ Matrix* Classificator::getWd() {
 
 void Classificator::saveParameterMatrix(const std::string& filePath, unsigned p)
 {
+    //Formatp .pgm
     std::ofstream output;
     output.open(filePath);
 
@@ -34,10 +35,21 @@ void Classificator::saveParameterMatrix(const std::string& filePath, unsigned p)
     output << "28 28\n";
     output << "65535\n";
 
+    double maior = 0;
     for (unsigned i = 0; i < 28; i++) {
         for (unsigned j = 0; j < 28; j++) {
-            //std::cout << "[" << i*28 + j << "][" << p << "]\n";
-            output <<  65535 - abs(65535 * (wd->at((i * 28 + j) + 1, p + 1))) << " ";
+            if (wd->at((i * 28 + j) + 1, p + 1) > maior)
+                maior = wd->at((i * 28 + j) + 1, p + 1);
+        }
+    }
+
+    // Essa correção é para melhorar a visualização da imagem, uma vez que os valores são normalizados.
+    double correcao;
+    correcao = 1 / maior;
+
+    for (unsigned i = 0; i < 28; i++) {
+        for (unsigned j = 0; j < 28; j++) {
+            output <<  65535 - abs(65535 * correcao * (wd->at((i * 28 + j) + 1, p + 1))) << " ";
         }
         output << "\n";
     }
